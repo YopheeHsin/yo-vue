1ca0d7a90aed17b1f1877d49669b3feb46098a62
辛跃飞 <yuefei.xin@yiducloud.cn>
2018-06-04 17:11:50 +0800

    collapse

--- src/pages/collapse.vue
+++ src/pages/collapse.vue
@@ -1,21 +1,24 @@
-<template>
-<div class="demo-collapse">
-	<yo-collapse-list v-model="activeNames" accordion>
-		<yo-collapse-item name="1">
-			<div slot="title">寻隐者不遇 <yo-icon name="map-pin" /></div>
-			<p>松下问童子，言师采药去。<br>
-			只在此山中，云深不知处。</p>
-		</yo-collapse-item>
-		<yo-collapse-item title="问刘十九" name="2">
-			<p>绿蚁新醅酒，红泥小火炉。<br>
-			晚来天欲雪，能饮一杯无？</p>
-		</yo-collapse-item>
-		<yo-collapse-item title="近试上张水部" name="3">
-			<p>洞房昨夜停红烛，待晓堂前拜舅姑。<br>
-			妆罢低声问夫婿，画眉深浅入时无。</p>
-		</yo-collapse-item>
-	</yo-collapse-list>
-</div>
+<template lang="pug">
+.demo-collapse
+	yo-collapse-list(v-model="activeNames" accordion)
+		yo-collapse-item(name="1")
+			.title(slot="title")
+				| 寻隐者不遇
+				yo-icon(name="map-pin")
+			p
+				| 松下问童子，言师采药去。
+				br
+				| 只在此山中，云深不知处。
+		yo-collapse-item(title="问刘十九" name="2")
+			p
+				| 绿蚁新醅酒，红泥小火炉。
+				br
+				| 晚来天欲雪，能饮一杯无？
+		yo-collapse-item(title="近试上张水部" name="3")
+			p
+				| 洞房昨夜停红烛，待晓堂前拜舅姑。
+				br
+				| 妆罢低声问夫婿，画眉深浅入时无。
 </template>
 
 <script>
@@ -24,11 +27,7 @@ import YoCollapseList from '../components/yo-collapse-list'
 import YoCollapseItem from '../components/yo-collapse-item'
 
 export default {
-	components: {
-		YoIcon,
-		YoCollapseList,
-		YoCollapseItem
-	},
+	components: { YoIcon, YoCollapseList, YoCollapseItem },
 
 	data() {
 		return {
@@ -40,6 +39,9 @@ export default {
 
 <style lang="sass">
 .demo-collapse
+	.title
+		.yo-icon
+			padding-left: 5px
 	p
 		margin: 0
 		font-size: 14px

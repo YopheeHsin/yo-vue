de2ab8a9db0819266db9f7303f3fdafe6a196c3d
辛跃飞 <yuefei.xin@yiducloud.cn>
2018-06-07 20:13:01 +0800

    update

--- src/pages/element/tabs.vue
+++ src/pages/element/tabs.vue
@@ -1,24 +1,24 @@
 <template lang="pug">
-//-
-	popper(ref="editPopper", :referenceElm="referenceElement", v-model="show.edit", :offset="-30", placement="bottom-start")
-	.project-group-edit-wrapper
-	el-input(v-model="tempQueue.name")
-	el-button.y-ml-sm(type="primary", @click="confirmEditQueue", :loading="loading.editQueue") 确定
-	el-button.y-ml-sm(@click="show.edit = false") 取消
-//
-
-el-tabs.project-tabs(v-model="activeIndex", type="card")
-	el-tab-pane(v-for="(queue, index) in queues" :key="index" :label="queue.name")
-		.project-tab__name(slot="label")
-			| {{ queue.name }}
-			el-dropdown.project-tab__action
-				i.el-icon-more
-				el-dropdown-menu(slot="dropdown")
-					el-dropdown-item(@click.native="editQueue(queue, index)") 编辑
-					el-dropdown-item(v-if="queues.length > 1", @click.native="deleteQueue(queue, index)") 删除
-	el-tab-pane
-		.project-tab__name--add(slot='label' @click.stop="addQueue(queues.length)")
-			i.el-icon-plus
+.editable-tabs
+	el-popover(v-model="visible.editQueue" placement="bottom")
+		el-input(v-model="activeIndex")
+		el-button(type="primary" @click="confirmEditQueue" :loading="loading.editQueue") 确定
+		el-button(@click="visible.editQueue = false") 取消
+	el-tabs.project-tabs(v-model="activeIndex", type="card")
+		el-tab-pane(v-for="(queue, index) in queues" :key="index" :label="queue.name")
+			.project-tab__name(slot="label")
+				| {{ queue.name }}
+				el-dropdown.project-tab__action
+					i.el-icon-more
+					el-dropdown-menu(slot="dropdown")
+						el-dropdown-item(@click.native="editQueue(queue, index)") 编辑
+						el-dropdown-item(
+							v-if="queues.length > 1"
+							@click.native="deleteQueue(queue, index)"
+						) 删除
+		el-tab-pane
+			.project-tab__name--add(slot='label' @click.stop="addQueue(queues.length)")
+				i.el-icon-plus
 </template>
 
 <script>
@@ -34,7 +34,29 @@ export default {
 				name: '当君怀归日'
 			}, {
 				name: '是妾断肠时'
-			}]
+			}],
+			loading: {
+				editQueue: false
+			},
+			visible: {
+				editQueue: true
+			}
+		}
+	},
+
+	methods: {
+		deleteQueue (queue, index) {
+			this.$confirm(`确认删除${queue.name}?`).then(() => {
+				this.queues.splice(index, 1)
+			})
+		},
+		addQueue (length) {
+			this.queues.push({
+				name: 'new' + (length + 1)
+			})
+		},
+		confirmEditQueue () {
+
 		}
 	}
 }
@@ -65,10 +87,15 @@ export default {
 		.el-icon-more
 			transform: rotate(90deg)
 			color: #999
+//覆盖
+.el-tabs__nav
+	height: 41px
+	overflow: hidden
 .el-tabs__item
-	margin-right: 10px
 	background-color: #f9f9f9
+	user-select: none
 	&.is-active
+		height: 41px
 		background-color: #fff
 		.project-tab__action
 			display: block

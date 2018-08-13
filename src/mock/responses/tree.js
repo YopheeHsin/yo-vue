ae54148e2f67f3110b327218fd74595cd8270b61
辛跃飞 <yuefei.xin@yiducloud.cn>
2018-08-13 15:36:50 +0800

    d3 tree

--- src/mock/responses/tree.js
+++ src/mock/responses/tree.js
@@ -1,11 +1,31 @@
+// export default {
+// 	size: [1, 1],
+// 	children: [{
+// 		size: [2, 4]
+// 	}, {
+// 		size: [3, 1],
+// 		children: [{
+// 			size: [4, 1]
+// 		}, ],
+// 	}, ],
+// }
+
 export default {
-	size: [1, 1],
-	children: [{
-		size: [2, 4]
+	"name": "A1",
+	"children": [{
+		"name": "B1",
+		"children": [{
+			"name": "C1",
+			"value": 100
+		}, {
+			"name": "C2",
+			"value": 300
+		}, {
+			"name": "C3",
+			"value": 200
+		}]
 	}, {
-		size: [3, 1],
-		children: [{
-			size: [4, 1]
-		}, ],
-	}, ],
+		"name": "B2",
+		"value": 200
+	}]
 }
--- src/pages/advanced/d3-flextree.vue
+++ src/pages/advanced/d3-flextree.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
 .flextree-container
 	svg
+		g.links
+		g.nodes
 </template>
 
 <script>
@@ -20,19 +22,52 @@ export default {
 		draw(data) {
 			this.setCanvasContainer()
 
-			const layout = flextree()
-			const tree = layout.hierarchy(data)
-			layout(tree)
 
-			console.log(tree)
+
+			const layout1 = flextree()
+			const tree1 = layout1.hierarchy(JSON.parse(JSON.stringify(data)))
+			console.log(tree1)
+
+			const tree2 = d3.hierarchy(JSON.parse(JSON.stringify(data)))
+			console.log(tree2)
+
+
+
+			// layout(tree)
+			// console.log(tree)
 
 				// .nodeSize(node => {
 				// 	return [node.width || 1, node.height || 1]
 				// })
 
 			// let node = hierarchy.descendants()
+			// this.nodeEnter(tree)
 
-			this.nodeEnter(tree)
+			// var root = d3.hierarchy(data)
+			// var treeLayout = d3.tree()
+			// treeLayout.size([400, 200])
+			// treeLayout(root)
+
+			// d3.select('svg g.nodes')
+			// 	.selectAll('circle.node')
+			// 	.data(root.descendants())
+			// 	.enter()
+			// 	.append('circle')
+			// 	.classed('node', true)
+			// 	.attr('cx', function(d) {return d.x})
+			// 	.attr('cy', function(d) {return d.y})
+			// 	.attr('r', 4)
+
+			// d3.select('svg g.links')
+			// 	.selectAll('line.link')
+			// 	.data(root.links())
+			// 	.enter()
+			// 	.append('line')
+			// 	.classed('link', true)
+			// 	.attr('x1', function(d) {return d.source.x})
+			// 	.attr('y1', function(d) {return d.source.y})
+			// 	.attr('x2', function(d) {return d.target.x})
+			// 	.attr('y2', function(d) {return d.target.y})
 		},
 
 		setCanvasContainer() {
@@ -40,7 +75,7 @@ export default {
 			const svg = d3.select('svg')
 				.attr('width', flextreeContainer.clientWidth)
 				.attr('height', flextreeContainer.clientHeight)
-			this.container = svg.append('g')
+			// this.container = svg.append('g')
 		},
 
 		// nodeInit(node) {
@@ -72,4 +107,11 @@ export default {
 	right: 30px
 	bottom: 30px
 	left: 30px
+	/deep/ .node
+		fill: steelblue
+		stroke: none
+	/deep/ .link
+		fill: none
+		stroke: #ccc
+		stroke-width: 1px
 </style>
